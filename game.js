b987c121c415ee801b28fe852d83b09c44939fd5
Zeeshan Qureshi <102156601+Zeefly121@users.noreply.github.com>
2024-08-15 11:47:29 +0500

    Create function to get player choice

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let number = Math.random() * 100;
 
@@ -10,3 +13,12 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    let choice = prompt("What is your choice? (1 = Rock, 2 = Paper, 3 = Scissors)").toLowerCase();
+
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        return console.log(choice);
+    } else {
+        return console.log("Please check your spelling or enter a valid input.");
+    }
+}
